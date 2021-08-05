173fca92761dc4dc194850dbf5fbe2d2f9618d21
01angee28 <85885635+01angee28@users.noreply.github.com>
2021-08-05 12:01:37 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,25 @@
+class Ground {
+
+    constructor(x,y,width,height){
+
+        var option = {
+            isStatic:true,
+            restitution:0,
+            friciton:0,
+            density:1     
+    }
+
+    this.body = Bodies.rectangle(x,y,width,height,option);
+    this.width = width;
+    this.height = height;
+    World.add(world,this.body);
+    }
+
+    display (){
+      var  pos = this.body.position;
+
+        rectMode(CENTER);
+        rect(pos.x,pos.y,this.width,this.height);
+        }
+
+}
---
+++ paper.js
@@ -0,0 +1,25 @@
+class Paper {
+
+    constructor(x,y,radius){
+
+        var option = {
+            isStatic:false,
+            restitution:0.3,
+            friciton:0.5,
+            density:1.2   
+    }
+
+    this.body = Bodies.circle(x,y,radius,option);
+    this.radius = radius;
+
+    World.add(world,this.body);
+    }
+
+    display (){
+     var   pos = this.body.position;
+
+        ellipseMode(RADIUS);
+        ellipse(pos.x,pos.y,this.radius,this.radius);
+        }
+
+}
